9cbd5eed018bdada204fd6ca4d02b10e002eed11
Nathaniel <nfgolding@gmail.com>
2023-12-06 13:42:08 +0100

    added a consent banner and consent management partial. Sends consent data to Google Tag Manager (also swapped Google Anayltics tag to Google Tag Manager Tag)

--- static/js/gtag.js
+++
@@ -1,8 +0,0 @@
-// <!-- Google tag (gtag.js) -->
-src="https://www.googletagmanager.com/gtag/js?id=G-3LEDHWN8YY";
-
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-3LEDHWN8YY');
